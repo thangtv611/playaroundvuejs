d3f78dfe5832bf28af2c6657782432bf83a0d8c4
Thang Tran <thangtv611@gmail.com>
2021-08-14 09:36:20 +0700

    Initial commit

---
+++ App.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <b-button size="sm" @click="toggle">
+      {{ show ? "Hide" : "Show" }} Alert
+    </b-button>
+    <b-alert v-model="show" class="mt-3" dismissible @dismissed="dismissed">
+      Hello {{ name }}!
+    </b-alert>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      name: "BootstrapVue",
+      show: true,
+    };
+  },
+  watch: {
+    show(newVal) {
+      console.log("Alert is now " + (newVal ? "visible" : "hidden"));
+    },
+  },
+  methods: {
+    toggle() {
+      console.log("Toggle button clicked");
+      this.show = !this.show;
+    },
+    dismissed() {
+      console.log("Alert dismissed");
+    },
+  },
+};
+</script>
+
+<style>
+body {
+  padding: 1rem;
+}
+</style>
